b537885dd754d41ecf0c1ded96c9fa1d72206954
Denis Malinochkin <mrmlnc@yandex-team.ru>
2017-08-27 16:37:45 +0300

    refactor(benchmark): Use globby instead of glob for benchmarking

--- src/benchmark/suites/10-sync-node-globby.ts
+++ src/benchmark/suites/10-sync-node-globby.ts
@@ -1,17 +1,17 @@
 import * as path from 'path';
 
-import * as glob from 'glob';
+import * as globby from 'globby';
 
 const cwd = path.join(process.cwd(), '.benchmark', process.env.BENCHMARK_CWD);
 
 console.time('timer');
 
-glob('**/*.md', { cwd, ignore: ['**/50000/**'] }, (err, matches) => {
-	if (err) {
-		console.error(err);
-		process.exit(0);
-	}
+try {
+	const matches = globby.sync('**/*.md', { cwd, ignore: ['**/50000/**'] });
 
 	console.info('files: ' + matches.length);
 	console.timeEnd('timer');
-});
+} catch (err) {
+	console.error(err);
+	process.exit(0);
+}
